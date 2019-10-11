86e1b1842f59ef69cdd97b93e25988363d5de486
Richard James Cavalcante Guimarães Ferreira <richard.ferreira@radixeng.com.br>
2019-10-11 17:43:40 -0300

    Usando Polimorfismo e Interface

---
+++ CursoPOO/banco/Banco.java
@@ -0,0 +1,20 @@
+
+public class Banco {
+
+	public static void main(String[] args) {
+		 ContaCorrente cc = new ContaCorrente(1020, 9090);
+		 cc.deposita(100.0);
+		 
+		 SeguroDeVida seguro = new SeguroDeVida();
+		 
+		 CalculadorDeImposto calc = new CalculadorDeImposto();
+		 calc.registra(cc);
+		 calc.registra(seguro);
+		 
+		 
+		 System.out.println(calc.getTotalImposto());
+		 
+
+	}
+
+}
