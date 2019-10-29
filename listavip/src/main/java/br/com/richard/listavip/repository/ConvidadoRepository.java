2afa97070529056f5d61cd8b54fcf5d0da1813e8
Richard James Cavalcante Guimarães Ferreira <richard.ferreira@radixeng.com.br>
2019-10-29 16:33:28 -0300

    Lista de convidados usando Spring Boot

---
+++ listavip/src/main/java/br/com/richard/listavip/repository/ConvidadoRepository.java
@@ -0,0 +1,9 @@
+package br.com.richard.listavip.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.richard.listavip.model.Convidado;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, Long>{
+
+}
